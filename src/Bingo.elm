61cc7cde3b9bb93cc04abb376b68ccf53f585fa4
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-02-18 01:40:00 +0700

    test in

--- src/Bingo.elm
+++ src/Bingo.elm
@@ -1,9 +1,17 @@
-module Bingo where
+module Bingo (..) where
 
 import Html
 import String
 
-main =
-    "bingo"
+title message times =
+  let go = 100 in
+  message ++ " "
     |> String.toUpper
+    |> String.repeat times
+    |> String.reverse
+    |> String.trimRight
     |> Html.text
+
+main =
+  title "bingo" 3
+
